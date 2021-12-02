1753dd0f4dc6aec4965ba4e141da59e1f301bf06
Tiago Pereira <tiagogomespereira@gmail.com>
2021-12-02 21:06:18 +0100

    Initial Commit

---
+++ NASAAPOD/NASAAPODApp.swift
@@ -0,0 +1,17 @@
+//
+//  NASAAPODApp.swift
+//  NASAAPOD
+//
+//  Created by Tiago Pereira on 02/12/21.
+//
+
+import SwiftUI
+
+@main
+struct NASAAPODApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
